5010c4c8069451387c7d70e6fa55754b7e22cb29
bymars <liubei447@gmail.com>
2012-12-28 13:44:01 +0800

    Add charpter6/k/ based on charpter6/j/

---
+++ chapter6/k/include/type.h
@@ -0,0 +1,15 @@
+/**********************************************************************
+ * type.h
+ * author: liu, linhong
+ **********************************************************************/
+
+#ifndef _ORANGES_TYPE_H_
+#define _ORANGES_TYPE_H_
+
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+typedef void (*int_handler) ();
+typedef void (*task_f) ();
+#endif /* _ORANGES_TYPE_H_*/
