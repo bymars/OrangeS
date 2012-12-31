73cef9e5801ffd9fb86e92c4a9e66173a7485546
bymars <liubei447@gmail.com>
2012-12-31 10:44:25 +0800

    Add charter6/m/ based on chapter6/l/

---
+++ chapter6/m/kernel/proc.c
@@ -0,0 +1,21 @@
+/********************************************************
+ * proc.c
+ * author: liu, linhong
+ ********************************************************/
+
+#include "type.h"
+#include "const.h"
+#include "protect.h"
+#include "proto.h"
+#include "string.h"
+#include "proc.h"
+#include "global.h"
+
+/*==================================================*
+		sys_get_ticks
+ *==================================================*/
+PUBLIC int sys_get_ticks()
+{
+	disp_str("+");
+	return 0;
+}
---
+++ chapter6/m/kernel/syscall.asm
@@ -0,0 +1,18 @@
+; ---------------------------------------------------
+; syscal..asm
+; author: liu, linhong
+; ---------------------------------------------------
+%include "sconst.inc"
+
+_NR_get_ticks	equ 	0
+INT_VECTOR_SYS_CALL equ 0x90
+
+global get_ticks
+
+bits 32
+[section .text]
+
+get_ticks:
+	mov eax, _NR_get_ticks
+	int INT_VECTOR_SYS_CALL
+	ret
