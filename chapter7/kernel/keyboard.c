27ce4150e6c1ad0e402fb5160839f56a7abccb6e
bymars <liubei447@gmail.com>
2013-02-05 13:40:47 +0800

    Complete chapter/e in book
    Handle shift alt and ctrl

--- chapter7/kernel/keyboard.c
+++ chapter7/kernel/keyboard.c
@@ -9,6 +9,18 @@
 #include "keymap.h"
 
 PRIVATE KB_INPUT kb_in;
+
+PRIVATE	int	code_with_E0 = 0;
+PRIVATE	int	shift_l;	/* l shift state */
+PRIVATE	int	shift_r;	/* r shift state */
+PRIVATE	int	alt_l;		/* l alt state	 */
+PRIVATE	int	alt_r;		/* r left state	 */
+PRIVATE	int	ctrl_l;		/* l ctrl state	 */
+PRIVATE	int	ctrl_r;		/* l ctrl state	 */
+PRIVATE	int	caps_lock;	/* Caps Lock	 */
+PRIVATE	int	num_lock;	/* Num Lock	 */
+PRIVATE	int	scroll_lock;	/* Scroll Lock	 */
+PRIVATE	int	column;
 /*==================================================*
 		keyboard_handler
  *==================================================*/
@@ -49,6 +61,9 @@ PUBLIC void keyboard_read()
 	u8 scan_code;
 	char output[2];
 	int make;
+
+	u32 key;
+	u32 *keyrow;
 	
 	memset(output, 0, 2);
 	if (kb_in.count > 0) 
@@ -66,11 +81,51 @@ PUBLIC void keyboard_read()
 		/* 下面开始解析扫描码 */
 		if (scan_code == 0xE1) {
 		} else if (scan_code == 0XE0) {
-
+			code_with_E0 = 1;
 		} else {
+			
 			make = (scan_code & FLAG_BREAK ? FALSE : TRUE);
-			if (make) {
-				output[0] = keymap[(scan_code & 0x7F) * MAP_COLS];
+			keyrow = &keymap[(scan_code & 0x7F) * MAP_COLS];
+			column = 0;
+			if (shift_l || shift_r) {
+				column = 1;	
+			}
+			if (code_with_E0) {
+				column = 2;
+				code_with_E0 = 0;
+			}
+			key = keyrow[column];
+			switch(key) {
+				case SHIFT_L :
+					shift_l = make;
+					key = 0;
+					break;	
+				case SHIFT_R :
+					shift_r = make;
+					key = 0;
+					break;
+				case CTRL_L :
+					ctrl_l = make;
+					key = 0;
+					break;
+				case CTRL_R :
+					ctrl_r = make;
+					key = 0;
+					break;
+				case ALT_L :
+					alt_l = make;
+					key = 0;
+					break;
+				case ALT_R :
+					alt_r = make;
+					key = 0;
+					break;
+				default :
+					if (!make) key = 0;
+					break;
+			}
+			if (key) {
+				output[0] = key;
 				disp_str(output);
 			}
 		}
