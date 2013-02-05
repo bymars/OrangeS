626fe6a49471fde24f0419bd1e9fc4262ca250ad
bymars <liubei447@gmail.com>
2013-02-05 14:41:03 +0800

    Complete chapter7/f in book
    handle printscreen and pausebreak button.
    Both keys has scancodes more than 2.

--- chapter7/kernel/keyboard.c
+++ chapter7/kernel/keyboard.c
@@ -21,6 +21,8 @@ PRIVATE	int	caps_lock;	/* Caps Lock	 */
 PRIVATE	int	num_lock;	/* Num Lock	 */
 PRIVATE	int	scroll_lock;	/* Scroll Lock	 */
 PRIVATE	int	column;
+
+PRIVATE u8	get_byte_from_kbuf();
 /*==================================================*
 		keyboard_handler
  *==================================================*/
@@ -62,27 +64,53 @@ PUBLIC void keyboard_read()
 	char output[2];
 	int make;
 
-	u32 key;
+	u32 key = 0;
 	u32 *keyrow;
 	
 	memset(output, 0, 2);
 	if (kb_in.count > 0) 
 	{
-		disable_int();
-		scan_code = *(kb_in.p_tail);
-		kb_in.p_tail++;
-		if (kb_in.p_tail == kb_in.buf + KB_IN_BYTES)
-		{
-			kb_in.p_tail = kb_in.buf;
-		}
-		kb_in.count--;
-		enable_int();
-
+		code_with_E0 = 0;
+		scan_code = get_byte_from_kbuf();
 		/* 下面开始解析扫描码 */
 		if (scan_code == 0xE1) {
+			int i;
+			u8 pausebrk_scode[] = {0xE1, 0x1D, 0x45,
+					       0xE1, 0x9D, 0xC5};
+			int is_pausebreak = 1;
+			for (i = 1; i < 6; i++) {
+				if (get_byte_from_kbuf() != pausebrk_scode[i]) {
+					is_pausebreak = 0;
+					break;
+				}
+			}
+			if (is_pausebreak) {
+				key = PAUSEBREAK;
+			}
 		} else if (scan_code == 0XE0) {
-			code_with_E0 = 1;
-		} else {
+			scan_code = get_byte_from_kbuf();
+			if (scan_code == 0x2A) {
+				if (get_byte_from_kbuf() == 0xE0) {
+					if (get_byte_from_kbuf == 0x37) {
+						key = PRINTSCREEN;
+						make = 1;
+					}
+				}
+			
+			}
+			if (scan_code == 0xB7) {
+				if (get_byte_from_kbuf() == 0xE0) {
+					if (get_byte_from_kbuf() == 0xAA) {
+						key = PRINTSCREEN;
+						make = 0;
+					}
+				}
+			}
+			if (key == 0) {
+				code_with_E0 = 1;
+			}
+		} 
+		if ((key != PRINTSCREEN) && (key != PAUSEBREAK)) {
 			
 			make = (scan_code & FLAG_BREAK ? FALSE : TRUE);
 			keyrow = &keymap[(scan_code & 0x7F) * MAP_COLS];
@@ -131,4 +159,26 @@ PUBLIC void keyboard_read()
 		}
 		//disp_int(scan_code);
 	}
+}
+
+/*======================================================================*
+			    get_byte_from_kbuf
+ *======================================================================*/
+PRIVATE u8 get_byte_from_kbuf() 
+{
+	u8 scan_code;
+
+	while (kb_in.count <= 0) {}
+
+	disable_int();
+        scan_code = *(kb_in.p_tail);
+        kb_in.p_tail++;
+        if (kb_in.p_tail == kb_in.buf + KB_IN_BYTES)
+        {
+                kb_in.p_tail = kb_in.buf;
+        }
+        kb_in.count--;
+        enable_int();
+	
+	return scan_code;
 }
